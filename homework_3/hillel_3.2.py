1fcef869c99f5f0ff74d2b4c013d6625b032a852
Alex Grek <svetlovegas@gmail.com>
2021-06-28 15:41:17 +0300

    HW3 task_3.2

---
+++ homework_3/hillel_3.2.py
@@ -0,0 +1,15 @@
+# Multiplications
+number_m = 2
+
+multiplication = number_m * 2
+number_m *= 2
+
+print(multiplication, number_m)
+
+# Divisions
+number_d = 2
+
+division = number_d / 2
+number_d /= 2
+
+print(division, number_d)
