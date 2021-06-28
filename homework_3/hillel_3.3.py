fa8d747607869489a1921228f489eef7d1b7ee05
Alex Grek <svetlovegas@gmail.com>
2021-06-28 15:41:37 +0300

    HW3 task_3.3

---
+++ homework_3/hillel_3.3.py
@@ -0,0 +1,10 @@
+friends = ["John", "Marta", "James"]
+enemies = ["John", "Jonatan", "Artur"]
+
+for friend in friends:
+    if friend == 'James':
+        print(f'{friend} we are the best friends.')
+    elif friend not in enemies:
+        print(f'{friend} we are the best friends.')
+    else:
+        print(f'{friend} we are not the friends anymore.')
