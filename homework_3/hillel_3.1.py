55638e8a753b7594dda2990f255d85b020fadc81
Alex Grek <svetlovegas@gmail.com>
2021-06-29 11:21:17 +0300

    HW3 loop for number_list

--- homework_3/hillel_3.1.py
+++ homework_3/hillel_3.1.py
@@ -1,10 +1,12 @@
 numbers_list = [1, 2, 3, 4, 5, 6, 7, 8]
 
-new_list = []
+even_numbers_list = []
+not_even_numbers_list = []
 for index, value in enumerate(numbers_list):
     if index % 2 > 0 and index != 0:
-        new_list.append((index, value))
+        not_even_numbers_list.append((index, value))
     else:
-        continue
+        even_numbers_list.append((index, value))
 
-print(new_list)
+print(even_numbers_list)
+print(not_even_numbers_list)
