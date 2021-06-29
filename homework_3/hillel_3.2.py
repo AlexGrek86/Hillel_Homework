11a64486ec466cecfcf67f0ca13e935f50dc6b9e
Alex Grek <svetlovegas@gmail.com>
2021-06-29 11:22:18 +0300

    HW3 multiplications and divisions

--- homework_3/hillel_3.2.py
+++ homework_3/hillel_3.2.py
@@ -1,15 +1,10 @@
 # Multiplications
-number_m = 2
+number = 2
 
-multiplication = number_m * 2
-number_m *= 2
-
-print(multiplication, number_m)
+print(number * 2)
+print(number << 1)
 
 # Divisions
-number_d = 2
 
-division = number_d / 2
-number_d /= 2
-
-print(division, number_d)
+print(number / 2)
+print(number >> 1)
