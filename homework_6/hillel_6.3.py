fd2b8141b4f19a99a8391cc6e6d2da5f03e7b758
Alex Grek <svetlovegas@gmail.com>
2021-07-16 20:00:04 +0300

    HW6 task_6.3

---
+++ homework_6/hillel_6.3.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    sqrt_n = sqrt(n)
+    i = 2
+    while i <= sqrt_n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+
+print(is_prime(2))
