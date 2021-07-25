e709055808172777203f337318d347578c8e9568
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-25 14:45:43 +0300

    HW7: add described issue for both modules

--- homework_7/hillel_7.1.py
+++ homework_7/hillel_7.1.py
@@ -6,4 +6,8 @@ def custom_map(func, args: list) -> list:
     return result
 
 
+if __name__ == '__main__':
+    print(custom_map(lambda item: item * 2, [1, 2, 3, 4, 5]))
 
+# too much empty lines in the end of module
+# -1 point
--- homework_7/hillel_7.2.py
+++ homework_7/hillel_7.2.py
@@ -12,5 +12,8 @@ def custom_filter(func, args: list) -> list:
 
 
 
+if __name__ == '__main__':
+    print(custom_filter(lambda item: item > 5, [3, 4, 5, 6]))
 
-
+# too much empty lines in the end of module
+# -1 point
